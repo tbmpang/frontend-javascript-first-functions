bf36d027e8d9890d61b86230b86426df1f922c78
tbmpang <tbmpang@hotmail.com>
2023-10-26 19:03:19 +0200

    Version 1.0

--- functions.js
+++ functions.js
@@ -13,21 +13,42 @@ console.log(greeting);
 /* Opdracht 1 */
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
+function getSchool() {
+  return "Novi Hogeschool";
+}
 
+const thisSchool = getSchool();
 
-
+console.log(thisSchool);
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
+function compliment() {
+  return "Lekker bezig met die functies, Jan!"
+}
 
+console.log(compliment())
 
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
+ grades = [8 , 8.5 , 6 , 7]
 
-
-
+function getGrades() {
+   return grades
+}
+console.log(getGrades())
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+firstName = "Jan"
+lastName = "Janssen"
+fullName = firstName + " " + lastName
+
+function getdetails() {
+  return fullName
+}
+
+console.log(getdetails())
